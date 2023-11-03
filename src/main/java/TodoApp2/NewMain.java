29126cb19ff00af5ef6fc358d54d584dffcf7edf
Arthur Dimas <arthurg5.3107@gmail.com>
2023-11-03 16:11:40 -0300

    first commit

---
+++ src/main/java/TodoApp2/NewMain.java
@@ -0,0 +1,17 @@
+
+package TodoApp2;
+
+/**
+ *
+ * @author arthurvdimas
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
